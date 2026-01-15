f83a06c90a948907ce2c40461bed929218afe8dc
cheevatee <38664398+cheevatee@users.noreply.github.com>
2026-01-15 14:49:29 +0700

    Create upload_model.py

---
+++ LLM-fine-tuning-LoRA/upload_model.py
@@ -0,0 +1,12 @@
+import boto3
+import os
+
+def upload_folder_to_s3(local_dir, bucket_name, s3_folder):
+    s3 = boto3.client('s3')
+    for root, dirs, files in os.walk(local_dir):
+        for file in files:
+            local_path = os.path.join(root, file)
+            s3_path = os.path.join(s3_folder, os.path.relpath(local_path, local_dir))
+            s3.upload_file(local_path, bucket_name, s3_path)
+
+upload_folder_to_s3("./onnx_output", "my-model-bucket", "exports/qwen3-mars-onnx/")
